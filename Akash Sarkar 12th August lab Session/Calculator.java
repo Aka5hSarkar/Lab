043ecb4f1bbc241baf904073a1136ccb1654bbc1
Aka5hSarkar <98515020+Aka5hSarkar@users.noreply.github.com>
2022-08-12 18:56:00 +0530

    Add files via upload

---
+++ Akash Sarkar 12th August lab Session/Calculator.java
@@ -0,0 +1,18 @@
+//Q.5Write a Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers.
+class Calculator{
+public static void main(String []args){
+int a=125;
+int b=24;
+System.out.println("the two numbers :" + a + "," + b);
+int sum = a+b;
+System.out.println("summation = " + sum);
+int sub = a-b;
+System.out.println("substration = " + sub);
+int mul = a*b;
+System.out.println("multiplcation = " + mul);
+int div = a/b;
+System.out.println("quotient = " + div);
+int rem = a%b;
+System.out.println("remainder = " + rem);
+}}
+
