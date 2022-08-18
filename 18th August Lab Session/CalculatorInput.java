4ae9782b30671d9123773b5decc9d8667ed87567
Aka5hSarkar <98515020+Aka5hSarkar@users.noreply.github.com>
2022-08-19 01:02:02 +0530

    Update CalculatorInput.java

--- 18th August Lab Session/CalculatorInput.java
+++ 18th August Lab Session/CalculatorInput.java
@@ -6,19 +6,19 @@ public class CalculatorInput {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.print("input 1st integer : ");
-		int a=sc.nextInt();//taking input
+		int a=sc.nextInt();//taking input and assigning it to a
 		System.out.print("input 2nd integer : ");
-		int b=sc.nextInt();
-		int sum=a+b;
-		int sub=a-b;
-		int mul=a*b;
-		int ave=(a+b)/2;
-		System.out.println("Sum of two integers : "+sum);
-		System.out.println("Difference of two integers : "+ sub);
-		System.out.println("Product of two integers : "+mul);
-		System.out.println("Average of two integers : "+ave);
-		System.out.println("Distance of two integers : "+sub);
-		if(a>b) {
+		int b=sc.nextInt();//taking input and assigning it to b
+		int sum=a+b;//addition 
+		int sub=a-b;//substraction
+		int mul=a*b;//multiplication
+		int ave=(a+b)/2;//average
+		System.out.println("Sum of two integers : "+sum);//printing sum
+		System.out.println("Difference of two integers : "+ sub);//printing difference
+		System.out.println("Product of two integers : "+mul);//printing product
+		System.out.println("Average of two integers : "+ave);//printing average
+		System.out.println("Distance of two integers : "+sub);//printing distance
+		if(a>b) {                                             //if-else logic
 			System.out.println("Max integer : " +a );
 			System.out.println("Min integer : "+b);
 		}
