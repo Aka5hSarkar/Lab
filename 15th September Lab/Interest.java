400f0ad157a2331d21c8ff3465144fd3495819c4
Aka5hSarkar <98515020+Aka5hSarkar@users.noreply.github.com>
2022-09-16 11:58:58 +0530

    Add files via upload

--- 15th September Lab/Interest.java
+++ 15th September Lab/Interest.java
@@ -4,36 +4,39 @@ ICICI and AXIS banks could provide 8%, 7%, and 9% rate of interest*/
 
 package Lab15September;
 
-
+import java.util.Scanner;
 class Bank{
-	static float getRateOfInterest() {
-		return 0f;
+	static float getRateOfInterest(float p) {
+		return p;
 	}
 }
 class  SBI extends Bank{
-	static float getRateOfInterest() {
-		return 8f;
+	static float getRateOfInterest(float p,int t) {
+		return (p*t*8)/100;
 	}
 }
 class ICICI extends Bank{
-	static float getRateOfInterest() {
-		return 7f;
+	static float getRateOfInterest(float p,int t) {
+		return (p*t*7)/100;
 	}
 }
 class Axis extends Bank{
-	static float getRateOfInterest() {
-		return 9f;
+	static float getRateOfInterest(float p,int t) {
+		return (p*t*9)/100;
 	}
 }
 
 public class Interest {
 
 	public static void main(String[] args) {
-		System.out.println("main rate of interest : "+Bank.getRateOfInterest()+"%");
-		System.out.println("SBI rate of interest : "+SBI.getRateOfInterest()+"%");
-		System.out.println("ICICI rate of interest : "+ICICI.getRateOfInterest()+"%");
-		System.out.println("Axis rate of interest : "+Axis.getRateOfInterest()+"%");
-
+		Scanner sc=new Scanner(System.in);
+		System.out.print("enter amount : ");
+		float amount=sc.nextFloat();
+		System.out.print("enetr number of years : ");
+		int time=sc.nextInt();
+		System.out.println("main amount : "+Bank.getRateOfInterest(amount));
+		System.out.println("SBI interest : "+SBI.getRateOfInterest(amount,time));
+		System.out.println("ICICI interest : "+ICICI.getRateOfInterest(amount,time));
+		System.out.println("Axis interest : "+Axis.getRateOfInterest(amount,time));
 	}
-
 }
