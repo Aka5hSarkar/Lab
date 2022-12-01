89c856c65665649cf2760a8e88556a2466ca163a
Aka5hSarkar <98515020+Aka5hSarkar@users.noreply.github.com>
2022-12-02 03:05:40 +0530

    Add files via upload

---
+++ ID_Not_Found_Exception.java
@@ -0,0 +1,12 @@
+package dec_1st_lab;
+
+public class ID_Not_Found_Exception extends Exception{
+	ID_Not_Found_Exception(String str){
+		super(str);
+	}
+}
+class InvalidSalaryException extends Exception{
+	InvalidSalaryException(String str){
+		super(str);
+	}
+}
