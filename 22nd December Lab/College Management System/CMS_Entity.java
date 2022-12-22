eb532f28fe0ba2b0638c135182ac4b41e49d7654
Aka5hSarkar <98515020+Aka5hSarkar@users.noreply.github.com>
2022-12-22 19:53:23 +0530

    Add files via upload

---
+++ 22nd December Lab/College Management System/CMS_Entity.java
@@ -0,0 +1,28 @@
+package com.cms.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name="new_cms_table")
+public class CMS_Entity {
+	@Id
+	private int id;
+	@Column(name="fName",length=20,nullable=false)
+	private String fName;
+	@Column(name="lName",length=20,nullable=false)
+	private String lName;
+	@Column(name="fees")
+	private double fees;
+	@Column(name="phone",length=11,unique=true)
+	private long ph;
+}
